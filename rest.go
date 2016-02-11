836db512e61c3eb25ad3b9dfd4ef324f9c4fc378
¨Mario <mario.vizcainos@gmail.com>
2016-02-10 21:36:56 -0500

    generando un json de ejemplo

--- rest.go
+++ rest.go
@@ -1,4 +1,43 @@
 package main 
+import(
+  "encoding/json"
+  "fmt"
+  "net/http"
+)
+type Payload struct{
+	Stuff Data
+}
+type Data struct{
+	Fruit Fruits
+	Veggies Vegetables
+}
+type Fruits map[string]int
+type Vegetables map[string]int
+
+func serveRest(w http.ResponseWriter, r * http.Request) {
+	response,err := getJsonResponse()
+	if err != nil {
+		panic(err);
+	}else{
+		fmt.Fprintf(w,string(response))
+	}
+}
 func main() {
-	
+	http.HandleFunc("/", serveRest)
+	http.ListenAndServe("localhost:1337",nil)
+
+}
+func getJsonResponse()([]byte,error) {
+	fruits := make(map[string]int)
+	fruits["Apples"] = 25
+	fruits["Organges"] = 10
+
+	vegetables := make(map[string]int)
+	vegetables["Carrots"] = 0
+	vegetables["Peppers"] = 15
+
+	d := Data{fruits,vegetables}
+	p := Payload{d}
+
+	return json.MarshalIndent(p,"","  ")
 }
